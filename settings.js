bb9a6138f064c856717941fe8e35ea4f761e2414
emmygamer <emmanuelimobio@gmail.com>
2024-09-22 15:00:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,9 +11,9 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
+global.botname = 'EMMY-GAMER-BOTZZ' //ur bot name
 global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.ownername = 'EMMY-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
